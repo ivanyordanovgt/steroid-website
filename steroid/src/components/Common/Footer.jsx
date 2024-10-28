96b0bf155ec8e0e1ae7e8ec644bdc6275835368a
Dimitar Velikov <trains4life420@abv.bg>
2024-10-28 11:39:12 +0200

    footer added

--- steroid/src/components/Common/Footer.jsx
+++ steroid/src/components/Common/Footer.jsx
@@ -3,7 +3,19 @@ import "./common.css";
 export const Footer = () => {
     return(
         <footer className="footer-container">
-            <h1>IT WORKS2</h1>
+            <nav>
+                <div className='nav-holder'>
+                    <ul className="nav">
+                        <li><a href='/products'>Shop products</a></li>
+                        <li><a href='#'>Quality control</a></li>
+                        <li><a href='#'>Verify</a></li>
+                        <li><a href='#'>Help Centre</a></li>
+                        <li><a href='/my-account'>My account</a></li>
+                    </ul>
+                </div>  
+                <span className="email-span"><a href="#">shop@kits4less.com</a></span>
+            </nav>
+             <span id="copyright-span">Copyright © Kits4Less.</span>
         </footer>
         )
 }
