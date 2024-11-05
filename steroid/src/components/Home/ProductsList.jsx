240c3910898c08b3eca3742dc315846f9c45000b
Dimitar Velikov <trains4life420@abv.bg>
2024-11-05 23:36:08 +0200

    added item description button, TODO: display actual information

--- steroid/src/components/Home/ProductsList.jsx
+++ steroid/src/components/Home/ProductsList.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import{ React, useState} from 'react'
 
 const ProductsList = ({products, titleLabel, addToCart}) => {
-    const testImageUrl = 'https://kits4less.com/wp-content/uploads/2024/05/standard-closed.png';
+  const testImageUrl = 'https://kits4less.com/wp-content/uploads/2024/05/standard-closed.png';
+  const [selectedProduct, setSelectedProduct] = useState(null);  // Stores the selected item's details
+
+  const handleViewProduct= (product) => {
+    setSelectedProduct(product);  // Set the selected item to display in popup
+  };
+
+  const closePopup = () => {
+    setSelectedProduct(null);  // Close the popup by clearing selected item
+  };
 
   return (
     <div className="products">
@@ -19,8 +28,19 @@ const ProductsList = ({products, titleLabel, addToCart}) => {
             <h2>${product.price.toFixed(2)}</h2>
             <h3 className="product-title">{product.title}</h3>
             <p className="product-text">{product.text}</p>
+            <button onClick={() => handleViewProduct(product)}>View Product</button>
             <button onClick={() => addToCart(product)}>Add to card</button>
           </div>
+
+          {selectedProduct && (
+            <div className="popup">
+              <div className="popup-content">
+                <h2>{selectedProduct.name}</h2>
+                <p>{selectedProduct.description}</p>
+                <button id='close-popup' onClick={closePopup}>Close</button>
+              </div>
+            </div>
+          )}
         </div>
       ))}
     </div>
